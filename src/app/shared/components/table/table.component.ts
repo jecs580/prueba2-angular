2e7218842e92dddc2931117628afb568777d3c5f
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-03-20 19:55:32 -0400

    Usando los datos de firebase en la tabla-post-admin y adicionando diseño de botones para agregar,editar y eliminar post

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { element } from 'protractor';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
+import { PostService } from './../../../components/post/post.service';
+import { Post } from '../../models/post.interface';
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -9,38 +12,58 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class TableComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['titlePost', 'tagsPost','Actions']; // Enparejamos con los ng-container
+  dataSource = new MatTableDataSource(); // Mostramos los datos en la tabla
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
-  constructor() { }
+  constructor(private postService: PostService) { }
   
   ngOnInit(): void {
+    this.postService.getAllPost().subscribe(
+      res => {
+        this.dataSource.data = res; // Cargamos los datos
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  ngAfterViewInit(){
+    // Se ejecuta cuando se inicia la visualizacion del component(Cuando se carga los estilos y la maquetacion)
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+
   }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
+  onNewPost(){
+    console.log('New post')
+  }
+  onEditPost(post:Post){
+    console.log('Edit', post);
+  }
+  onDeletePost(post:Post){
+    console.log(post);
+  }
 }
